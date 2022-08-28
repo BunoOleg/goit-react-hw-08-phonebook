543ddb300310af814aecd0dc9a06933c9654de2f
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-08-28 22:11:05 +0300

    Update authOperations.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -12,24 +12,17 @@ const token = {
     },
 }
 
-const register = createAsyncThunk('/auth/register', async (credentials, thunkAPI) => {
-    try {
-        const { data } = await axios.post('/users/signup', credentials);
-        token.set(data.token);
-        return data;
-    } catch (error) {
-        if (error.response.data.name === 'MongoError') {
-            return thunkAPI.rejectWithValue('isRegisterEmailAlertShown');
-
-        };
-
-        if (error.response.data.errors.password.kind === 'minlength') { 
-            return thunkAPI.rejectWithValue('isRegisterPasswordAlertShown');
-        };
-
-    }
-
-});
+const register = createAsyncThunk('auth/register', async credentials => {
+  console.log(credentials);
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    console.log(data);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}); 
 
 const login = createAsyncThunk('/auth/login', async (credentials, thunkAPI) => {
     try {
