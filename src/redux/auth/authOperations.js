a5d2f2c2fc0b4577f734e100e07aa6b3357efc23
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-08-29 18:31:24 +0300

    1

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -13,15 +13,14 @@ const token = {
 }
 
 const register = createAsyncThunk('/auth/register', async (credentials, thunkAPI) => {
-    try {
-      const { data } = await axios.post('/users/signup', credentials);
-      token.set(data.token);
-      return data;
-    } catch (error) {
-      console.log(error.message);
-    }
-
-});
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};);
 
 const login = createAsyncThunk('/auth/login', async (credentials, thunkAPI) => {
     try {
